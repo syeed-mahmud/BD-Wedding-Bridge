23bd5080128495c4e321aad12f4d5b826ed8d5ad
anjumnr99 <anjumanowara99@gmail.com>
2024-06-09 03:14:43 +0600

    landing page updated

--- Back End/header.php
+++ Back End/header.php
@@ -23,11 +23,11 @@
         <li class="nav-item">
         <div class="dropdown dropdown-end">
           <div tabindex="0" role="button" class="btn m-1">Setting</div>
-             <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-               <li><a href="#">Profile</a></li>
-               <li><a  href="../Back End/logout.php">Log In</a></li>
-               <li><a  href="../Back End/logout.php">Admin Log In</a></li>
-               <li><a  href="../Back End/logout.php">Log out</a></li>
+             <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 ">
+               <li><a class="btn" href="#">Profile</a></li>
+               <li><a class="btn" href="../Back End/logout.php">Log In</a></li>
+               <li><a class="btn" href="../Back End/logout.php">Admin Log In</a></li>
+               <li><a class="btn" href="../Back End/logout.php">Log out</a></li>
           </ul>
           </div>
         </li>
