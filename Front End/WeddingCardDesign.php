65da9778ad1f148f38cb5f85bc97fa5801e36dc8
syeed-mahmud <syeed.mahmud@northsouth.edu>
2024-06-10 01:21:37 +0600

    Invitation card Backend Connect

--- Front End/WeddingCardDesign.php
+++ Front End/WeddingCardDesign.php
@@ -1,16 +1,41 @@
 <?php
+session_start();
+
+// Database connection
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "wedding_bridge";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+if (!isset($_SESSION['user_id']) && ($_SESSION['selected_wedid'])) {
+    header("Location: ../Front End/login.html");
+    exit();
+}
+
+$selected_wedid = $_SESSION['selected_wedid'];
+$user_id = $_SESSION['user_id'];
+
+
+$query = $conn->prepare("SELECT DISTINCT bg.G_first_name, bg.B_first_name, w.event_location, w.event_start_date, u.name
+FROM `bride-groom` AS bg 
+JOIN weddingevent AS w ON bg.WedReg_id = w.WedReg_id 
+JOIN regesterd_wedding AS r ON bg.WedReg_id = r.Wed_id 
+JOIN users AS u ON u.id = r.user_id
+WHERE bg.WedReg_id = ?");
+$query->bind_param("s", $selected_wedid);
+$query->execute();
+$query->bind_result(
+    $G_first_name,$B_first_name, $event_location, $event_start_date,$name
+);
+
+$query->fetch();
+$query->close();
 ?>
 
 <!DOCTYPE html>
@@ -31,12 +56,12 @@ if ($conn->connect_error) {
           <img src="../images/is-yours-to-conquer-3.png"/>
           <img class="is-yours-to-conquer" src="../images/is-yours-to-conquer-1.png" />
           <img class="img" src="../images/is-yours-to-conquer-2.png" />
-          <div class="text-wrapper">Syeed</div>
+          <div class="text-wrapper"><?php echo htmlspecialchars($name) ?></div>
           <p class="you-are-invited-to">You Are Invited To The Weeding Of</p>
           <img class="line" src="../images/line-7.svg" />
-          <div class="ayman-munjerin">Ayman<br />&amp;<br />munjerin</div>
-          <div class="date-nov">Date : 23/nov/2024</div>
-          <div class="div">Location : Dhaka, Bangladesh</div>
+          <div class="ayman-munjerin"><?php echo htmlspecialchars($G_first_name) ?><br />&amp;<br /><?php echo htmlspecialchars($B_first_name) ?></div>
+          <div class="date-nov">Date : <span><?php echo htmlspecialchars($event_start_date) ?></span></div>
+          <div class="div">Location : <span><?php echo htmlspecialchars($event_location) ?></span></div>
           <div class="BD-WEDDING-BRIDGE">Bd Wedding Bridge</div>
           <img class="element" src="../images/Logo.png" />
         </div>
