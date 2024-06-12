61354e2883e8739dac3867d55cd1e3b2be2dda60
syeed-mahmud <syeed.mahmud@northsouth.edu>
2024-06-12 17:32:16 +0600

    short change

--- Front End/profile.php
+++ Front End/profile.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 }
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: ../Front End/login.html");
+    header("Location: ../Front End/login.php");
     exit();
 }
 
